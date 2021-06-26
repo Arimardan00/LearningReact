47852889f33a257a7fd03efa89bd15cf8ee178fa
Arimardan00 <32637776+Arimardan00@users.noreply.github.com>
2021-06-26 23:20:56 +0530

    Rendering Lists & Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,40 +1,48 @@
-import React, {useState} from 'react';
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from "react";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
-import './Expenses.css';
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2019');
+  const [filteredYear, setFilteredYear] = useState("2020");
+  
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
-}
+};
 
 export default Expenses;
+
+/** 1. Outputting condition content using && operator
+ * {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+ */
+
+/** 2. Outputting condition content using ternary(?:) operator
+ * {filteredExpenses.length === 0 ? <p>No expenses found.</p> : <filteredExpenses...}
+ */
+
+/** 3. Outputting Condition Content
+ * let expensesContent = <p>No expenses found.</p>;
+ * if (filteredExpenses.length > 0) {
+ *  expensesContent = filteredExpenses.map((expense) => (
+ *    <ExpenseItem
+ *      key={expense.id}
+ *      title={expense.title}
+ *      amount={expense.amount}
+ *      date={expense.date} 
+ */
